812922653ffed2ad4ee6800d23b21508f3c15939
shailraj24 <rajshailraj24@gmail.com>
2022-09-01 16:43:07 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// Program for user Reg.
+// let userName = "learncodeonline";
+// let full_Name = "Anurag";
+// const eMail = "qwerty@gmail.com";
+// let Phone_Num = 1234567890;
+// let pass = "qwer";
+
+// console.log(typeof Phone_Num);
+
+// console.log("My username is " + userName + " My Mobile Number is" + Phone_Num);
+
+// console.log(`
+// My User Name : ${userName},
+//     My Mobile Number is: ${Phone_Num}
+// `);
+
+// Operators
+// let value1 = 25;
+// let value2 = 4;
+
+// let result = value1 % value2;
+// console.log(result);
+
+// let mrp = 1999;
+// let discprice = 699;
+// console.log("The MRP of item is " + mrp);
+// let DPrice = ((mrp - discprice) / mrp) * 100;
+// console.log("The Discounted Percentage is " + DPrice);
+// let roundprice = Math.round(DPrice);
+// console.log("The Discounted percentage is " + roundprice);
+
+// let an = null;
+// console.log(typeof an);
+
+// let ab = 5;
+// let abc = ++ab;
+// console.log(abc);
+
+// let var1 = 7;
+// let var2 = "7";
+
+// console.log(var1 === var2);
+// console.log(var1 == var2);
+
+// let string = "elonmusk";
+// string = string.split("");
+// string = string.reverse();
+// string = string.join("");
+// console.log(string);
+
+// const array = ["react" , "anguler" , "vue"];
+// const i = array.values();
+// // i.next();
+// // console.log(i.next());
+
+// const name = (...args) => {
+//     let sum = 0;
+//     for (let element of args){
+//         sum = sum + element;
+//     }
+//     return sum;
+// };
+
+
+
+// console.log(name(1,2,3,4,5,6,7,8,9,34));

687d9f058af0c40a9c15ffe63018039095bd7fae
shailraj24 <rajshailraj24@gmail.com>
2022-09-02 11:32:19 +0530

    add function

--- tp.js
+++ tp.js
@@ -1,1 +1,22 @@
 console.log("Hellow");
+
+var stage = function (posetion){
+    switch (posetion) {
+        case "owner":
+            console.log(`shailraj is owner`);
+            break;
+        case "partner":
+            console.log(`shailraj is partner`);
+            break;
+        case "invester":
+            console.log(`shailraj is investere`);
+            break;
+    
+        default:
+            console.log(`shailraj is visitor`);
+            break;
+    }
+
+}
+
+stage("owner")
